8f6c47df642a5d84946732b94f2cae1dcdd84574
Wang Yuen <wangdemo2018@gmail.com>
2025-06-30 23:15:59 -0400

    fix credential

--- base_web_app/main.tf
+++ base_web_app/main.tf
@@ -3,8 +3,6 @@
 ##################################################################################
 
 provider "aws" {
-  access_key = "ACCESS_KEY"
-  secret_key = "SECRET_KEY"
   region     = "us-east-1"
 }
 
